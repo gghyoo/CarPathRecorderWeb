67f9b60d8ae2cbbdf05835428f2bd08d98a556dc
Gavin <ggh@imscv.com>
2016-02-11 11:15:30 +0800

    更新了上传类

--- Application/Recorder/Controller/IndexController.class.php
+++ Application/Recorder/Controller/IndexController.class.php
@@ -9,28 +9,39 @@ class IndexController extends Controller {
     	$this->show("GPS Location Recorder");
     }
 
-    public function display($id){
+    public function getRecordById($id){
     	$Location = M($this->TABLE_NAME);
-    	$result = $Location->find($id);
-    	if($result){
-    		dump($result);
-    	}
+    	$result = $Location->find($id);        
+        echo $result ? json_encode($result) : "Failed";
+    }
+
+    public function getRecords($offset = -1, $length = -1){
+        $Location = M($this->TABLE_NAME);
+        if($length == -1)
+            $result = $Location->order("id")->select();
+        else
+            $result = $Location->order("id")->limit($offset, $length)->select();
+        echo $result ? json_encode($result) : "Failed";
     }
 
     public function add(){    	
-        $JsonData = I('post.JsonData','');
-        echo $JsonData;
-        $Data = json_decode($JsonData, TRUE);
-        dump($Data);
-		//$Location = M($this->TABLE_NAME);
-		//$Location->create();
-		//$Location->add();
-		//$this->show("OK");
+        $JsonData = I('post.JsonData','','');
+        $DataList = json_decode($JsonData, TRUE);
+        if($DataList)
+        {
+            $Location = M($this->TABLE_NAME);
+            $Location->create();
+            $Location->addAll($DataList);
+            echo "Ok";
+            return;
+        }
+        
+        echo "Failed";		
     }
 
     public function del($id){
     	$Location = M("location");
 		$Location->where("id=".$id)->delete();
+        echo "Ok";
     }
-
 }
